caa91310e825c6373b0b9d05ce085e848bdb8c76
Mason Keiser <masonksr5@gmail.com>
2020-11-05 04:28:47 +0000

    removed unnecessary spacing

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -44,7 +44,7 @@ export default class Login extends React.Component {
                 <input type="text" name='email' id='user_email' className="mr-3 pr-1"  onChange={this.handleChange}/>
               </div>
               <div className='form-group password'>
-                <label htmlFor="password" className="pl-3 ml-4">Pass:     </label>
+                <label htmlFor="password" className="pl-3 ml-4">Pass: </label>
                 <input type="text" name='password' className="ml-2 pl-2" id='user_password' onChange={this.handleChange}/>
               </div>
               <div className='form-group'>
